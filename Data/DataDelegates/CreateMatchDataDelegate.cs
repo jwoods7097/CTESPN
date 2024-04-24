dcf51f7bf98df81cdcf979826f66e0c3b11ee126
innyun <vinnysun1@gmail.com>
2024-04-24 15:22:50 -0500

    small backend updates

---
+++ Data/DataDelegates/CreateMatchDataDelegate.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DataDelegates
+{
+    internal class CreateMatchDataDelegate : NonQueryDataDelegate<Match>
+    {
+        public readonly string Location;
+        public readonly DateOnly Date;
+        public readonly int Attendance;
+
+        public CreateMatchDataDelegate(string location, DateOnly date, int attendance) : base("MLS.CreateMatch")
+        {
+            Location = location;
+            Date = date;
+            Attendance = attendance;
+        }
+
+        public override void PrepareCommand(Command command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("Location", Location);
+            command.Parameters.AddWithValue("Date", Date);
+            command.Parameters.AddWithValue("Attendance", Attendance);
+
+            var p = command.Parameters.Add("MatchID", System.Data.SqlDbType.Int);
+            p.Direction = System.Data.ParameterDirection.Output;
+        }
+
+        public override Match Translate(Command command)
+        {
+            return new Match(command.GetParameterValue<int>("MatchID"), Location, Date, Attendance)
+        }
+    }
+}
--- Data/Models/Match.cs
+++ Data/Models/Match.cs
@@ -4,10 +4,10 @@ namespace Data.Models
     {
         public int MatchID { get; }
         public string Location { get; }
-        public DateTime Date { get; }
+        public DateOnly Date { get; }
         public int Attendance { get; }
 
-        public Match(int matchID, string location, DateTime date, int attendance)
+        public Match(int matchID, string location, DateOnly date, int attendance)
         {
             MatchID = matchID;
             Location = location;
