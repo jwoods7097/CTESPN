88de322349ed372444b2fcd56a3006175286524b
jwoods7097 <jwoods7097@gmail.com>
2024-04-30 23:37:21 -0500

    Fix null goalkeeper issue

--- Data/SQL/Procedures/MLS.RetrievePlayers.sql
+++ Data/SQL/Procedures/MLS.RetrievePlayers.sql
@@ -1,11 +1,11 @@
 CREATE OR ALTER PROCEDURE MLS.RetrievePlayers
 AS
 SELECT P.PlayerID, P.PlayerTypeID, P.[Name],
-    CASE 
-        WHEN P.PlayerTypeID = 4 THEN NULL
-        ELSE O.Position
-    END AS Position
+    IIF(P.PlayerTypeID = 4, 'Goalkeeper', O.Position)
 FROM MLS.Player P
     LEFT JOIN MLS.Goalkeeper G ON P.PlayerID = G.PlayerID AND P.PlayerTypeID = G.PlayerTypeID
     LEFT JOIN MLS.Outfielder O ON P.PlayerID = O.PlayerID AND P.PlayerTypeID = O.PlayerTypeID
+ORDER BY P.Name ASC
 GO
+
+EXEC MLS.RetrievePlayers
